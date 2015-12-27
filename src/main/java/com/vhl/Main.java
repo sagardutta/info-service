2ffd6716ee0d0b8ad074ce61008560ed9aee1a47
Dutta <sdutta@plnl13b0a575d.attlocal.net>
2015-12-26 22:12:00 -0600

    starting server once and for all

--- src/main/java/com/vhl/Main.java
+++ src/main/java/com/vhl/Main.java
@@ -40,8 +40,8 @@ public class Main {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+//        System.in.read();
+//        server.stop();
     }
 }
 
